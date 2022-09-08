17f3364ff162a93ddca96d322a6a964856dee307
John Matthew Shenk <jmshenk1@millersville.edu>
2022-09-08 11:28:26 -0400

    MongoDB Methods
    Added some functions to retrieve a clientURI and client for use in our program.

--- Application/src/Interface.java
+++ Application/src/Interface.java
@@ -1,6 +1,11 @@
-// Name:		Working Title
-// Authors: 	John Shenk, Benjamin Slinghoff, Lauryn Simmons, Alex Peiffer, Meba Shimelis
-// Description:	A program used for adding classes to a database and displaying them in a orderly fashion.
+/*
+ * @projectDescription	A program for adding classes to a database
+ * 
+ * @authors	John Shenk, Benjamin Slinghoff, Lauryn Simmons, Alex Peiffer, Meba Shimelis
+ * @version 0.0.1
+ * @dateLastModified September 8, 2022
+ */
+
 
 import java.io.*;
 
@@ -48,4 +53,45 @@ import static com.mongodb.client.model.Projections.*;
 
 public class Interface {
 	
+	//***** INFORMATION FOR THOSE UNFAMILIAR WITH MONGODB *****//
+	/*
+	 * I highly recommend downloading MongoDB Compass and putting in the connection string 
+	 * 	to be able to view the database
+	 * Do NOT change the connection string declared at the top of the main function
+	 */
+	public static void main(String[] args) throws IOException {
+		// Connect to database using a connection String - DO NOT CHANGE THIS
+		String connString = "mongodb+srv://JAMBL:se420@clusterjambl.4bnlbcs.mongodb.net/test";
+		
+		/*
+		 * Getting a MongoClient using a Client URI and the connection string
+		 * Used for retrieving info from DB
+		 */
+		MongoClientURI clientURI = getURI(connString);
+		MongoClient client = getClient(clientURI);
+		
+		
+	}
+	
+	/*
+	 * @name getURI
+	 * @description	Returns a Mongo Client URI using a connection string
+	 * @param conn	The connection string used to connect to the database
+	 * @return A new Mongo Client URI
+	 */
+	public static MongoClientURI getURI(String conn)
+	{
+		return new MongoClientURI(conn);
+	}
+
+	/*
+	 * @name getClient
+	 * @description	Returns a Mongo Client using a client URI
+	 * @param uri	The client URI used to get the client
+	 * @return A new Mongo Client
+	 */
+	public static MongoClient getClient(MongoClientURI uri)
+	{
+		return new MongoClient(uri);
+	}
 }
