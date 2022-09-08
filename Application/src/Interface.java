3e9e96a5374d2957b0fda153a20e2895c711a1e1
John Matthew Shenk <jmshenk1@millersville.edu>
2022-09-08 12:33:32 -0400

    Update Interface.java

--- Application/src/Interface.java
+++ Application/src/Interface.java
@@ -10,8 +10,11 @@
 import java.io.*;
 
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
+//import com.mongodb.MongoClient;
+//import com.mongodb.MongoClientURI;
+
+
+/*
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
@@ -49,7 +52,8 @@ import com.mongodb.client.MongoCursor;
 // Create Index 
 import com.mongodb.client.model.Indexes;
 //project fields 
-import static com.mongodb.client.model.Projections.*;
+import static com.mongodb.client.model.Projections.*
+*/
 
 public class Interface {
 	
@@ -58,18 +62,17 @@ public class Interface {
 	 * I highly recommend downloading MongoDB Compass and putting in the connection string 
 	 * 	to be able to view the database
 	 * Do NOT change the connection string declared at the top of the main function
-	 */
+	*/
 	public static void main(String[] args) throws IOException {
+		/*
 		// Connect to database using a connection String - DO NOT CHANGE THIS
 		String connString = "mongodb+srv://JAMBL:se420@clusterjambl.4bnlbcs.mongodb.net/test";
-		
 		/*
 		 * Getting a MongoClient using a Client URI and the connection string
 		 * Used for retrieving info from DB
 		 */
-		MongoClientURI clientURI = getURI(connString);
-		MongoClient client = getClient(clientURI);
-		
+		//MongoClientURI clientURI = getURI(connString);
+		//MongoClient client = getClient(clientURI);
 		
 	}
 	
@@ -79,10 +82,10 @@ public class Interface {
 	 * @param conn	The connection string used to connect to the database
 	 * @return A new Mongo Client URI
 	 */
-	public static MongoClientURI getURI(String conn)
-	{
-		return new MongoClientURI(conn);
-	}
+	//public static MongoClientURI getURI(String conn)
+	//{
+		//return new MongoClientURI(conn);
+	//}
 
 	/*
 	 * @name getClient
@@ -90,8 +93,8 @@ public class Interface {
 	 * @param uri	The client URI used to get the client
 	 * @return A new Mongo Client
 	 */
-	public static MongoClient getClient(MongoClientURI uri)
-	{
-		return new MongoClient(uri);
-	}
+	//public static MongoClient getClient(MongoClientURI uri)
+	//{
+		//return new MongoClient(uri);
+	//}
 }
