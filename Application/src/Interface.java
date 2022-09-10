9f2e11183112a4406e5dc85f259ab31efd84c840
John Matthew Shenk <jmshenk1@millersville.edu>
2022-09-09 20:26:07 -0400

    Added JSON import
    Added JSON import and .jar file

--- Application/src/Interface.java
+++ Application/src/Interface.java
@@ -16,47 +16,11 @@ import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 
-/*
+
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 
-import org.bson.Document;
-import org.bson.conversions.Bson;
-import org.bson.conversions.Bson;
-import com.mongodb.client.result.DeleteResult;
-import static com.mongodb.client.model.Filters.eq;
-import com.mongodb.BasicDBObject;
-import com.mongodb.client.MongoCursor;
-//Create Document Objects
-import org.bson.Document;
-//Create Bson Objects (e.g. Bson filters)
-import org.bson.conversions.Bson;
-import static com.mongodb.client.model.Accumulators.sum;
-import static com.mongodb.client.model.Accumulators.avg;
-import static com.mongodb.client.model.Aggregates.group;
-import static com.mongodb.client.model.Aggregates.limit;
-import static com.mongodb.client.model.Aggregates.match;
-import static com.mongodb.client.model.Aggregates.sort;
-import static com.mongodb.client.model.Aggregates.unwind;
-//eq method returns a Bson filter that tests if arg1 (logic) AND arg2
-import static com.mongodb.client.model.Filters.and;
-//and method returns a Bson filter that tests if arg1 = arg2
-import static com.mongodb.client.model.Filters.eq;
-//gt method returns a Bson filter that tests if arg1 > arg2
-import static com.mongodb.client.model.Filters.gt;
-import static com.mongodb.client.model.Filters.gte;
-import static com.mongodb.client.model.Filters.lt;
-import static com.mongodb.client.model.Filters.or;
-import static com.mongodb.client.model.Sorts.ascending;
-import static com.mongodb.client.model.Sorts.descending;
-import java.util.Arrays;
-//Create Cursor Object to visit all documents in one query result
-import com.mongodb.client.MongoCursor;
-// Create Index 
-import com.mongodb.client.model.Indexes;
-//project fields 
-import static com.mongodb.client.model.Projections.*
-*/
+import org.json.simple.JSONObject;
 
 public class Interface {
 	
@@ -107,7 +71,7 @@ public class Interface {
 	 * @name getClient
 	 * @description	Returns a Mongo Client using a client URI
 	 * @param uri	The client URI used to get the client
-	 * @return A new Mongo Client
+	 * @return A new Mongo Client 
 	 */
 	public static MongoClient getClient(MongoClientURI uri)
 	{
