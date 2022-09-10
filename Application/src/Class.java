47ebcbc117764f11c836e31e1a9038d8ae23ced1
Alex Peiffer <alexpeiffer72@gmail.com>
2022-09-10 19:50:10 -0400

    Implemented the addRelationship method in Class

--- Application/src/Class.java
+++ Application/src/Class.java
@@ -1,3 +1,25 @@
+import java.util.*;
+
 public class Class {
+
+    private String className;
+    private HashSet<Attribute> attributes = new HashSet<Attribute>();
+    private HashSet<Relationship> relationships = new HashSet<Relationship>();
+
+    //code for constructor method needed
+
+    //code for Attribute methods
+
+    // This method adds a Class to relationships with the Class added being the destination
+    // parameter Relationship
+    public void addRelationship (Relationship newRel) {
+
+        if (relationships.contains(newRel)) {
+            System.out.println ("Relationship already exists!");
+        } else {
+           relationships.add(newRel);
+        }
+
+    }
     
 }
