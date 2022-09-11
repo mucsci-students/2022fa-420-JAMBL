3eaac3d9bc0f681a1b9f270a177e925d77638742
m-s1621 <mebashimelis@gmail.com>
2022-09-10 20:02:45 -0400

    getter and setter for class and attribute class

--- Application/src/Class.java
+++ Application/src/Class.java
@@ -1,3 +1,28 @@
 public class Class {
+
+    //field
+    String name;
+    HashSet<Attribute> attributes; //set of Attribute of a Class
+	HashSet<Relationship> relationships; //set of Relationship of a Class
+
+    //constructor with name as parameter
+    public Class(name){
+        this.name = Name;
+    }
+
+   //get Class Name
+    public String getClassName(){
+
+        return name;
     
+    }
+    
+    
+    // set class Name
+    public void setClassName(String name){
+    
+        this.name = Name;
+    
+    }
+
 }
