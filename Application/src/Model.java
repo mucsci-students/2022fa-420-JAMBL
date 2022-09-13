95a1eac46a835ce2c352d109094f22330d840986
Lauryn Simmons <lssimmon@millersville.edu>
2022-09-13 00:06:08 -0400

    added addClass in Model

--- Application/src/Model.java
+++ Application/src/Model.java
@@ -1,3 +1,31 @@
+import java.util.*;
 public class Model {
-    
+
+    HashSet<Class> classes;
+
+    public Model(){
+        // when load() is working it will taking ins a HashSet
+        classes = new HashSet<Class>();
+    }
+    public void addClass (String className) {
+       
+        Iterator<Class> itClasses = classes.iterator();
+        Class aClass;
+        // iterates through the HashSet
+        while (itClasses.hasNext()) {
+            aClass = (Class) itClasses.next();
+            // if any of the class names match the notifies user
+            if (aClass.getClassName().equals(className)) {
+                System.out.println("Class already exists!");
+                // if class already exists exits method
+                return;
+            }
+        }
+        // creates new class and addes it to the classes set
+        Class newClass = new Class(className);
+        classes.add(newClass);
+        // notifies user
+        System.out.println("Class " + className + " added!");
+            
+    }
 }
