490252974ef9c55e817cf7e6a30762cb7d3e58bb
Benjamin Slinghoff <bslinghoff@gmail.com>
2022-09-24 14:39:25 -0400

    Updated relationship.java
    Added enum for the types of relationships, a setter and getter to set and get the type of relationship and changed the constructor to include the type

--- Application/src/Relationship.java
+++ Application/src/Relationship.java
@@ -17,14 +17,31 @@ public class Relationship {
     This class is the representation of a relationship between two Class classes.
     Stores the Class that is the destination of a relationship between two classes
     */
+
+        public enum Type {
+            AGGREGATION, COMPOSITION, INHERITANCE, REALIZATION; 
+        }
+        
         // destination is the Class that receives the relationship from the origin class
         private Class destination;
+
+        // relationshipType is the Type that this relationship is classified as
+        private Type relationshipType;
+
+        // sets the type of the relationship to the given string
+        public void setRelType(Type type){
+            this.relationshipType = type;
+        }
+
+        // returns the type of the relationship
+        public Type getRelType(){
+            return this.relationshipType;
+        }
     
-        // Constructor thats takes a Class as a parameter to set destination
-        public Relationship (Class destination) {
-    
+        // Constructor thats takes a Class as a parameter to set destination and a String to set type
+        public Relationship (Class destination, Type type) {
             this.destination = destination;
-    
+            this.relationshipType = type;
         }
     
         // a method to get the Class that is the destination of a Relationship
