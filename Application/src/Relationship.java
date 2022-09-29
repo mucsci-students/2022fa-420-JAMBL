968f6cd0ac94e2cba7c9e9c69e754019b1124a04
John Matthew Shenk <jmshenk1@millersville.edu>
2022-09-28 20:47:04 -0400

    Update Relationship.java

--- Application/src/Relationship.java
+++ Application/src/Relationship.java
@@ -1,5 +1,4 @@
 /*
-
 This class is the representation of a relationship between two Class classes.
 Stores the Class that is the destination of a relationship between two classes
 */
@@ -17,14 +16,45 @@ public class Relationship {
     This class is the representation of a relationship between two Class classes.
     Stores the Class that is the destination of a relationship between two classes
     */
+
+        public enum Type {
+            AGGREGATION("AGGREGATION"), 
+            COMPOSITION("COMPOSITION"), 
+            INHERITANCE("INHERITANCE"), 
+            REALIZATION("REALIZATION");
+
+            private String name;
+
+            private Type(String name) {
+                this.name = name;
+            }
+           
+            public String toString(){
+                return name;
+            }
+    
+        }
+        
         // destination is the Class that receives the relationship from the origin class
         private Class destination;
+
+        // relationshipType is the Type that this relationship is classified as
+        private Type relationshipType;
+
+        // sets the type of the relationship to the given string
+        public void setRelType(String type){
+            this.relationshipType = Type.valueOf(type);
+        }
+
+        // returns the type of the relationship
+        public String getRelType(){
+            return this.relationshipType.toString();
+        }
     
-        // Constructor thats takes a Class as a parameter to set destination
-        public Relationship (Class destination) {
-    
+        // Constructor thats takes a Class as a parameter to set destination and a String to set type
+        public Relationship (Class destination, String type) {
             this.destination = destination;
-    
+            this.relationshipType = Type.valueOf(type);
         }
     
         // a method to get the Class that is the destination of a Relationship
