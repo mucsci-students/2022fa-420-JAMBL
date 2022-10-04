87b70353eb464342472542df2b6caeed573fa3dc
m-s1621 <96027075+m-s1621@users.noreply.github.com>
2022-10-04 13:49:24 -0400

    check valid type for addrel and relType

--- Application/src/View.java
+++ Application/src/View.java
@@ -452,4 +452,8 @@ public class View {
         System.out.println("Relation type changed to " + typeName.toLowerCase()  + "!");
     }    
 
+    public void relTypeCheck(String typeName){
+        System.out.println(typeName + " is invalid Relation type !");
+    }    
+
 }
